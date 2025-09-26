f9315bc6576e3fcc6acedd641cbde9781c5bdf0a
dngatcha <bakouengit@gmail.com>
2025-09-26 15:39:00 +0200

    feat: Add the FrameDecoder

--- swigan/models/utils.py
+++ swigan/models/utils.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import torch
 from torch import nn
 
 
@@ -62,3 +63,46 @@ def single_conv_block(
     layers.append(nn.Dropout(dropout))
 
     return nn.Sequential(*layers)
+
+
+class SCSEModule(nn.Module):
+    """Implement a Spatial and Channel Squeeze Excitation (scSE) Module.
+
+    Incorporates Attention mechanism in the decoding phase.
+    See <https://arxiv.org/abs/1803.02579> for more details.
+    """
+
+    def __init__(self, in_channels: int, reduction: int = 16) -> None:
+        """Initialize the input class.
+
+        Args:
+        ----
+            in_channels: Number of input channels.
+            reduction: Reduction factor to apply. Reduces the number of input_channels
+                to input_channels // reduction.
+
+        """
+        super().__init__()
+        self.cSE = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, in_channels // reduction, 1),
+            nn.BatchNorm2d(in_channels // reduction),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels // reduction, in_channels, 1),
+            nn.Sigmoid(),
+        )
+        self.sSE = nn.Sequential(nn.Conv2d(in_channels, 1, 1), nn.Sigmoid())
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass.
+
+        Args:
+        ----
+            x: The input tensor. Should be of shape (batch_size, channels, height, width).
+
+        Returns:
+        -------
+            A tensor of shape (batch_size, channels, height, width)
+
+        """
+        return x * self.cSE(x) + x * self.sSE(x)
