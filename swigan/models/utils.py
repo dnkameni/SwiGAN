63b3586f67e7a88883ed10e958b67a682bae7b3c
dngatcha <bakouengit@gmail.com>
2025-10-06 15:25:15 +0200

    feat: Update Lightning moduel with scheduler, temporal discriminator, and infogan network

--- swigan/models/utils.py
+++ swigan/models/utils.py
@@ -2,8 +2,25 @@
 
 from __future__ import annotations
 
+from enum import Enum
+from typing import TypeAlias
+
 import torch
 from torch import nn
+
+Channels: TypeAlias = list[int]
+
+
+class ModelFlavour(Enum):
+    """Class containing the spatial encoder's/decoder's input/output channels.
+
+    Returns a tuple of lists of ints With the first and second element being
+    respectively the encoder and decoder channels.
+    """
+
+    Small: tuple[Channels, Channels] = ([8, 16, 32, 64], [64, 32, 16, 8])
+    Medium: tuple[Channels, Channels] = ([16, 32, 64, 128], [128, 64, 32, 16])
+    Large: tuple[Channels, Channels] = ([32, 64, 128, 256], [256, 128, 64, 32])
 
 
 def single_conv2d_block(
